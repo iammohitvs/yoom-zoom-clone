a08ef9eb0a7fecbe2578ddf382d0307f47245ade
Mohit V S <iammohitvs@Mohits-MacBook-Air.local>
2024-03-31 12:29:39 +0530

    finished stream create meeting

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
     return (
         <section className="flex size-full flex-col gap-10 text-white">
             <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
-                <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+                <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 md:p-11">
                     <h2 className="glassmorphism max-w-[270px] rounded py-2 text-center text-base font-normal">
                         Upcomin meeting at: 12:30 PM
                     </h2>
--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
+import { StreamVideoProvider } from "@/providers/StreamClientProvider";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
-    return <main>{children}</main>;
+    return (
+        <StreamVideoProvider>
+            <main>{children}</main>
+        </StreamVideoProvider>
+    );
 };
 
 export default RootLayout;
